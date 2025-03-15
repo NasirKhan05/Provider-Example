d453ac6ebc68327d6882354cda95705bd3165269
david <nasirkhan85690@gmail.com>
2025-03-15 14:36:31 +0500

    Changing Screen Mode To Dark Mode, Light Mode and System Mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_example/provider/count_provider.dart';
 import 'package:provider_example/provider/example_one_provider.dart';
+import 'package:provider_example/provider/theme_changer_provider.dart';
 import 'package:provider_example/screen/count_example.dart';
+import 'package:provider_example/screen/dark_theme.dart';
 import 'package:provider_example/screen/example_one.dart';
 
 void main() {
@@ -14,20 +16,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers:
-        [
+
+    return MultiProvider(
+        providers: [
           ChangeNotifierProvider(create: (_) => CountProvider()),
           ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
+          ChangeNotifierProvider(create: (_) => ThemeChanger()),
         ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: ExampleOne(),
-      ),
-    );
-    
+        child: Builder(builder: (BuildContext context) {
+          final themeChanger = Provider.of<ThemeChanger>(context);
+          return MaterialApp(
+            title: 'Provider Examples',
+            themeMode: themeChanger.themeMode,
+            theme: ThemeData(
+              brightness: Brightness.light,
+                primarySwatch: Colors.deepPurple,
+              appBarTheme: AppBarTheme(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white
+              )
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primarySwatch: Colors.yellow,
+              primaryColor: Colors.purple,
+              appBarTheme: AppBarTheme(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white
+              )
+            ),
+            home: DarkTheme(),
+          );
+        }));
   }
 }
